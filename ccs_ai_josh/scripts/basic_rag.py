a2412653445f7e2c86e50c92d3557deef5ffb145
SamuelHLewis <sam.lewis1@crowncommercial.gov.uk>
2025-07-07 18:01:33 +0100

    Add basic CLI

--- ccs_ai_josh/scripts/basic_rag.py
+++ ccs_ai_josh/scripts/basic_rag.py
@@ -1,11 +1,14 @@
 import os
 from dotenv import load_dotenv
 from langchain_community.vectorstores.azuresearch import AzureSearch
-from langchain_openai import AzureOpenAIEmbeddings
+from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
+from langchain import hub
+from langchain_core.documents import Document
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
 
 load_dotenv()
 
-# specify the embedding model
 embeddings: AzureOpenAIEmbeddings = AzureOpenAIEmbeddings(
     azure_deployment=os.getenv("EMBEDDING_DEPLOYMENT_NAME"),
     openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
@@ -13,23 +16,59 @@ embeddings: AzureOpenAIEmbeddings = AzureOpenAIEmbeddings(
     api_key=os.getenv("EMBEDDING_MODEL_KEY"),
 )
 print("Embedding model connected")
-# specify the vector store
+
 vector_store: AzureSearch = AzureSearch(
     azure_search_endpoint=os.getenv("VECTOR_STORE_ENDPOINT"),
     azure_search_key=os.getenv("VECTOR_STORE_KEY"),
     index_name=os.getenv("VECTOR_STORE_INDEX"),
     embedding_function=embeddings.embed_query,
-    fields=["document_id", "content", "url", "filename"]
+    content_key="chunk"
 )
 print("Vector store connected")
 
-# take input from command line as a surrogate for input from the front-end
-user_input = input("What do you want to know?\n")
-# Perform a similarity search
-# Note: this breaks at the moment because the vector store doesn't actually contain any embeddings, just keywords
-docs = vector_store.similarity_search(
-    query=user_input,
-    k=3,
-    search_type="similarity",
+llm = AzureChatOpenAI(
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    openai_api_key=os.getenv("AZURE_OPENAI_KEY"),
+    azure_deployment=os.getenv("DEPLOYMENT_NAME"),
+    openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    temperature=0.0
 )
-print(docs[0].page_content)
+print("LLM connected")
+
+# Define prompt for question-answering
+# N.B. for non-US LangSmith endpoints, you may need to specify
+# api_url="https://api.smith.langchain.com" in hub.pull.
+prompt = hub.pull("rlm/rag-prompt")
+
+# Define state for application
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+# Define application steps
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+# Compile application and test
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+def generate_response(question:str):
+    """Invoke the LLM graph to generate a response to a question.
+    """
+    response = graph.invoke({"question": question})
+    return(response["answer"])
+
+while 1<2:
+    print("\n")
+    user_input = input("What do you want to know?\n\n")
+    print(generate_response(question=user_input))
