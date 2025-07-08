4a6be2ad251502d98c34acfb977c3b068e7c8890
SamuelHLewis <sam.lewis1@crowncommercial.gov.uk>
2025-07-08 13:32:54 +0100

    Initial UI

--- app.py
+++ app.py
@@ -1,1 +1,50 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+from azure.search.documents.indexes import SearchIndexClient
+from azure.core.credentials import AzureKeyCredential
+from langchain_community.vectorstores.azuresearch import AzureSearch
+from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
+from typing_extensions import List, TypedDict
+from src.llm_utils import check_index_naming, generate_response
+
+st.set_page_config(layout="wide")
+
+st.title("AI Josh")
+
+load_dotenv()
+
+# before connecting to anything, check that the vector store fields are compatible with langchain
+index_client = SearchIndexClient(os.getenv("VECTOR_STORE_ENDPOINT"), AzureKeyCredential(os.getenv("VECTOR_STORE_KEY")))
+vector_store_name_status = check_index_naming(index_client=index_client, index_name=os.getenv("VECTOR_STORE_INDEX"))
+if vector_store_name_status == False:
+    raise Exception("Vector store naming is not compatible with LangChain")
+
+embeddings: AzureOpenAIEmbeddings = AzureOpenAIEmbeddings(
+    azure_deployment=os.getenv("EMBEDDING_DEPLOYMENT_NAME"),
+    openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("EMBEDDING_MODEL_ENDPOINT"),
+    api_key=os.getenv("EMBEDDING_MODEL_KEY"),
+)
+
+vector_store: AzureSearch = AzureSearch(
+    azure_search_endpoint=os.getenv("VECTOR_STORE_ENDPOINT"),
+    azure_search_key=os.getenv("VECTOR_STORE_KEY"),
+    index_name=os.getenv("VECTOR_STORE_INDEX"),
+    embedding_function=embeddings.embed_query,
+    content_key="chunk"
+)
+
+llm = AzureChatOpenAI(
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    openai_api_key=os.getenv("AZURE_OPENAI_KEY"),
+    azure_deployment=os.getenv("DEPLOYMENT_NAME"),
+    openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    temperature=0.0
+)
+
+user_input = st.chat_input("How can I help?")
+
+if user_input:
+    response = generate_response(question=user_input, vector_store=vector_store, llm=llm)
+    st.write(response)
