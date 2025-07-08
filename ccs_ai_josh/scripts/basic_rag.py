b6f3085f2a27fc1740d9ac1b7f01aa13e542b702
SamuelHLewis <sam.lewis1@crowncommercial.gov.uk>
2025-07-08 12:34:33 +0100

    Add check for langchain compatibility

--- ccs_ai_josh/scripts/basic_rag.py
+++ ccs_ai_josh/scripts/basic_rag.py
@@ -1,13 +1,24 @@
 import os
 from dotenv import load_dotenv
+from azure.search.documents.indexes import SearchIndexClient
+from azure.core.credentials import AzureKeyCredential
 from langchain_community.vectorstores.azuresearch import AzureSearch
 from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
 from langchain import hub
 from langchain_core.documents import Document
 from langgraph.graph import START, StateGraph
 from typing_extensions import List, TypedDict
+from src.llm_utils import check_index_naming
 
 load_dotenv()
+
+# before connecting to anything, check that the vector store fields are compatible with langchain
+index_client = SearchIndexClient(os.getenv("VECTOR_STORE_ENDPOINT"), AzureKeyCredential(os.getenv("VECTOR_STORE_KEY")))
+vector_store_name_status = check_index_naming(index_client=index_client, index_name=os.getenv("VECTOR_STORE_INDEX"))
+if vector_store_name_status:
+    print("Vector store is named correctly")
+else:
+    raise Exception("Vector store naming is not compatible with LangChain")
 
 embeddings: AzureOpenAIEmbeddings = AzureOpenAIEmbeddings(
     azure_deployment=os.getenv("EMBEDDING_DEPLOYMENT_NAME"),
