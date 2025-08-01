efab86810e5515709fa87b6fb7eb0524d279cdc9
SamuelHLewis <sam.lewis1@crowncommercial.gov.uk>
2025-08-01 15:01:09 +0100

    Add loop to evaluate responses for first entries in truthset

--- ccs_ai_josh/scripts/basic_eval.py
+++ ccs_ai_josh/scripts/basic_eval.py
@@ -1,9 +1,35 @@
 import os
+import pandas as pd
 from dotenv import load_dotenv
-from langchain_openai import AzureChatOpenAI
+from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
+from azure.search.documents.indexes import SearchIndexClient
+from azure.core.credentials import AzureKeyCredential
+from langchain_community.vectorstores.azuresearch import AzureSearch
+from src.llm_utils import check_index_naming, generate_response
 from src.eval_utils import evaluate_response
 
 load_dotenv()
+
+# before connecting to anything, check that the vector store fields are compatible with langchain
+index_client = SearchIndexClient(os.getenv("VECTOR_STORE_ENDPOINT"), AzureKeyCredential(os.getenv("VECTOR_STORE_KEY")))
+vector_store_name_status = check_index_naming(index_client=index_client, index_name=os.getenv("VECTOR_STORE_INDEX"))
+if vector_store_name_status == False:
+    raise Exception("Vector store naming is not compatible with LangChain")
+
+embeddings: AzureOpenAIEmbeddings = AzureOpenAIEmbeddings(
+    azure_deployment=os.getenv("EMBEDDING_DEPLOYMENT_NAME"),
+    openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("EMBEDDING_MODEL_ENDPOINT"),
+    api_key=os.getenv("EMBEDDING_MODEL_KEY"),
+)
+
+vector_store: AzureSearch = AzureSearch(
+    azure_search_endpoint=os.getenv("VECTOR_STORE_ENDPOINT"),
+    azure_search_key=os.getenv("VECTOR_STORE_KEY"),
+    index_name=os.getenv("VECTOR_STORE_INDEX"),
+    embedding_function=embeddings.embed_query,
+    content_key="chunk"
+)
 
 # Connect to model on Azure
 llm = AzureChatOpenAI(
@@ -14,22 +40,55 @@ llm = AzureChatOpenAI(
     temperature=0.0
 )
 
-# example data
-q = "What is the capital of France?"
-ref_ans = "The capital of France is Paris."
-gen_ans_correct = "Paris is the capital of France."
-gen_ans_incorrect = "Paris is the capital of Spain"
-chunk1 = "European capitals: UK=London, France=Paris, Spain=Madrid, Germany=Berlin"
-chunk2 = "French cities: Marseille, Paris (capital), Lyon, Versailles"
-chunk3 = "Spanish cities: Barcelona, Madrid, Seville"
+# truthset needs to be downloaded from Google Drive and placed in `data` folder
+truthset_file_path = os.path.join('data', 'AI Josh Truthset.xlsx')
+truthset_sheet_name = 'Questions'
 
-results = evaluate_response(
-    llm=llm,
-    question=q,
-    response=gen_ans_correct,
-    context=[chunk1, chunk2],
-    ref_answer=ref_ans
-)
-print(f"Correctness = {results["correctness"]}")
-print(f"Retrieval = {results["retrieval"]}")
-print(f"Groundedness = {results["groundedness"]}")
+# Read the sheet into a pandas DataFrame
+truthset = pd.read_excel(truthset_file_path, sheet_name=truthset_sheet_name)
+# take first 5 for debugging, delete this step when everything is working
+truthset = truthset.head(2)
+# send each question to the model and store the response in the truthset df
+responses = []
+for i in truthset['Question']:
+    response = generate_response(question=i, vector_store=vector_store, llm=llm)
+    responses.append(response)
+truthset["Answer"] = [i['answer'] for i in responses]
+truthset["Retrieved Files"] = [i['source_names'] for i in responses]
+truthset["Retrieved Contents"] = [i['source_contents'] for i in responses]
+# evaluate each response
+# THIS IS SENSITIVE TO THE STRUCTURE OF THE TRUTHSET SPREADSHEET
+# if the order of columns changes, the iloc statements need to be reviewed
+correctness = []
+retrieval = []
+groundedness = []
+for i in range(len(truthset)):
+    # print(f"Question: {truthset.iloc[i,3]}")
+    # print(f"Ref answer: {truthset.iloc[i,4]}")
+    # print(f"LLM answer: {truthset.iloc[i,-1]}")
+    # print(f"LLM context: {responses[i]['source_contents']}")
+    score = evaluate_response(
+        llm=llm,
+        question=truthset.iloc[i,3],
+        response=truthset.iloc[i,-1],
+        context=responses[i]['source_contents'],
+        ref_answer=truthset.iloc[i,4]
+    )
+    correctness.append(score['correctness'])
+    retrieval.append(score['retrieval'])
+    groundedness.append(score['groundedness'])
+# store the scores in the truthset df
+truthset['Correctness'] = correctness
+truthset['Retrieval'] = retrieval
+truthset['Groundedness'] = groundedness
+# rearrange columns to put context columns on right, because these have loads of content
+columns = list(truthset.columns)
+cols_to_move = ['Retrieved Files', 'Retrieved Contents']
+for col in cols_to_move:
+    columns.remove(col)
+columns.extend(cols_to_move)
+truthset = truthset[columns]
+
+# write results to file, including all columns in the truthset file
+# deliberately not erasing the original truthset file
+truthset.to_csv(os.path.join('data', 'evaluation_results.csv'), index=False)
